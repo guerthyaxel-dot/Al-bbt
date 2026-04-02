154b31b21e1abb80d5990a7b087ebc675396a639
ɪ'ᴍ ᴅɪᴇɢᴏ ᜊ•ᴗ•ᜊ <sarmientodieg@gmail.com>
2026-04-02 10:56:16 -0500

    Actualizar database.ts

--- core/system/database.ts
+++ core/system/database.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const DB_PATH = path.join(process.cwd(), 'lib', 'database.json');
+const DB_PATH = path.join(process.cwd(), 'core', 'database.json');
 
 let database = {
   users: {},
